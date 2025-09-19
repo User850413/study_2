cf4fd2da4830185ed6d234082a059c4f7c73303d
User850413 <csyou1228@gmail.com>
2025-09-19 10:59:03 +0900

    25/09/19 :sparkles: : IconButton

--- src/Pages/Field/index.jsx
+++ src/Pages/Field/index.jsx
@@ -3,20 +3,30 @@ import "../../styles/Field.scss"
 import { Fragment } from "react";
 import BackpackIcon from '@mui/icons-material/Backpack';
 import IconButton from "../../components/common/IconButton";
+import SettingsIcon from '@mui/icons-material/Settings';
+import HomeIcon from '@mui/icons-material/Home';
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
 
 const Field = () => {
-
 
     return (
         <Fragment>
             <div className="headerWrapper">
                 <ul>
                     <li>
+                        <IconButton icon={<BackpackIcon />} isActivable areaLabel="inventory" />
                     </li>
-                    <li>b</li>
+                    <li>
+                        <IconButton icon={<AccessibilityIcon />} isActivable areaLabel="user" />
+                    </li>
+                    <li>
+                        <IconButton icon={<SettingsIcon />} isActivable areaLabel="setting" />
+                    </li>
                 </ul>
                 <div>
-                    <Link to="/">메인으로</Link>    
+                    <Link to="/">
+                        <IconButton icon={<HomeIcon />} areaLabel="toHome" />
+                    </Link>    
                 </div>
             </div>
         </Fragment>
--- src/components/common/IconButton.jsx
+++ src/components/common/IconButton.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import { useCallback, useEffect, useState } from "react";
+import "./IconButton.scss"
+import clsx from "clsx";
 
-const IconButton = ({icon, size}) => {
+const IconButton = ({areaLabel, icon, size, isActivable=false, onClick}) => {
+    const [ isActive, setIsActive ] = useState(false);
+
+    if (!areaLabel) console.warn("areaLabel should be added")
+    if (!size) size = "md"
+    if (!icon) console.error("no icon provided");
+    if (!isActivable && !onClick) onClick = () => { console.log("btn clicked but nothing happend") }
+    if (onClick && typeof onClick !== "function") console.error("onClick's type must be function");
+
+    // 추가된 onClick
+    const addedOnClick = onClick;
+
+    const handleClick = useCallback(() => {
+        if(typeof addedOnClick === 'function'){
+            addedOnClick();
+        }else if(!isActivable){
+            console.log("btn clicked but no onClick provided");
+        }
+
+        if(isActivable) setIsActive(prev => !prev);
+    }, [addedOnClick, isActivable])
+
     return (
-        <div>
-            
-        </div>
+        <button 
+            aria-label={areaLabel} 
+            onClick={handleClick}
+            className={clsx(
+                "iconBtn",
+                size,
+                isActivable && isActive && "active",
+            )}
+            >            
+            {icon}
+        </button>
     );
 };
 
