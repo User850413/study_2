f9a0479d819ec12d50485c900c5ab6b7f1f9f092
User850413 <csyou1228@gmail.com>
2025-09-19 10:20:48 +0900

    025/09/19 :package: : mui 설치

--- src/Pages/Field/index.jsx
+++ src/Pages/Field/index.jsx
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
+import "../../styles/Field.scss"
+import { Fragment } from "react";
+import BackpackIcon from '@mui/icons-material/Backpack';
+import IconButton from "../../components/common/IconButton";
 
 const Field = () => {
 
 
     return (
-        <div>
-            <div>
-                <Link to="/">메인으로</Link>    
+        <Fragment>
+            <div className="headerWrapper">
+                <ul>
+                    <li>
+                    </li>
+                    <li>b</li>
+                </ul>
+                <div>
+                    <Link to="/">메인으로</Link>    
+                </div>
             </div>
-        </div>
+        </Fragment>
     );
 };
 
---
+++ src/components/common/IconButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IconButton = ({icon, size}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default IconButton;
