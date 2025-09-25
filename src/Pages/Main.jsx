aea2b8d2695d8f2a3ad9f22e1daea65b6891fbe3
s000h99@winitech.com <S000h99@winitech.com>
2025-09-25 17:46:59 +0900

    25/09/25 :sparkles: : 유저데이터 로컬에 저장

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,10 +1,55 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import InfoProvider from '../context/InfoProvider';
+import DefaultForm from '../components/UI/Form/DefaultForm';
+import DefaultInput from '../components/UI/Form/DefaultInput';
+import DefaultBtn from '../components/UI/Form/DefaultBtn';
+import { useState } from 'react';
+
+const USER_DATAKEYS = ["name"];
+
+const DEFAULT_USER = {
+    name:""
+}
 
 const Main = () => {
+    const [userData, setUserData] = useState(() => {
+        const rawData = localStorage.getItem("userData");
+        if(!rawData) return DEFAULT_USER;
+
+        const parsedData = JSON.parse(rawData);
+        USER_DATAKEYS.forEach((d) => {
+            if(!(d in parsedData)) alert("잘못된 유저명이 저장되어 있습니다.");
+            localStorage.removeItem("userData");
+        })
+        return parsedData;
+    });
+
+    const navigate = useNavigate();
+
+    const onChangeUserData = (e) => {
+        setUserData(prev => ({...prev, [e.target.name]:e.target.value}));
+    }
+
+    const goToMain = () => {
+        navigate("/field");
+    }
+
+    const onClickGoToMain = () => {
+        localStorage.setItem("userData", JSON.stringify(userData));
+        goToMain();
+    }
+
     return (
         <div>
-            <Link to={"/field"}>들어가기</Link>
+            <ul>
+                <li>
+                    <label htmlFor="input-name">이름</label>
+                    <input type="text" name="name" id="input-name" value={userData.name} onChange={onChangeUserData} />
+                </li>
+            </ul>
+            <button type="button" onClick={onClickGoToMain}>
+                들어가기
+            </button>
         </div>
     );
 };
